8b0438363133fbdb5e76bc6ad463773163eec3e2
Alex McLain <alex@alexmclain.com>
2014-03-03 19:57:17 -0800

    Added Context#socket.

--- lib/0mq/context.rb
+++ lib/0mq/context.rb
@@ -8,6 +8,12 @@ module ZMQ
       @ptr = LibZMQ.zmq_ctx_new
     end
     
+    # Create a Socket within this context.
+    def socket(type, opts={})
+      opts[:context] = self
+      ZMQ::Socket.new type, opts
+    end
+    
   end
   
   DefaultContext = Context.new
