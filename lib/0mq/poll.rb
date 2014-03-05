7315b22096a2c3fbcbe609440f9fe811b6244a33
Alex McLain <alex@alexmclain.com>
2014-03-04 21:13:39 -0800

    Implemented Poll#run.

--- lib/0mq/poll.rb
+++ lib/0mq/poll.rb
@@ -13,8 +13,9 @@ module ZMQ
     
     # Construct a Poll object and start polling.
     # See #initialize for parameters.
+    # See #run for block and return value.
     def self.poll(*sockets, &block)
-      new(*sockets, &block).tap { |poll| poll.run }
+      new(*sockets).tap { |poll| poll.run(&block) }
     end
     
     # Accepts a list of sockets to poll for events
@@ -33,65 +34,67 @@ module ZMQ
     #
     # Example:
     # ZMQ::Poll.new socket1, socket2, socket3 => ZMQ::POLLOUT, timeout: 1
-    def initialize(*sockets, &block)
+    def initialize(*sockets)
       opts = sockets.last.is_a?(Hash) ? sockets.pop : {} # For Ruby 1.9
       
       @timeout = opts.fetch :timeout, -1
       
-      poll_items = []
-      socks = {}
+      @poll_items = []
+      @socks = {}
       
-      sockets.each { |socket| socks[socket] = ZMQ::POLLIN }
+      sockets.each { |socket| @socks[socket] = ZMQ::POLLIN }
       
       # Pull remaining sockets out of options hash and package into poll items.
       # Skip any option symbols in the hash; they aren't sockets.
       # Rejecting symbols allows duck-typed sockets to be included.
-      socks.merge! opts.reject {|socket, events| socket.is_a? Symbol}
+      @socks.merge! opts.reject {|socket, events| socket.is_a? Symbol}
       
       # Build table to reference ZMQ::Socket to its pointer's address.
       # This is an easy way to reconnect PollItem to ZMQ::Socket without
       # having to store multiple dimensions in the socks hash.
-      socket_lookup = {}
-      socks.each { |socket, event| socket_lookup[socket.ptr.address] = socket }
+      @socket_lookup = {}
+      @socks.each { |socket, event| @socket_lookup[socket.ptr.address] = socket }
       
       # Allocate space for C PollItem (zmq_pollitem_t) structs.
-      poll_pointer_array = FFI::MemoryPointer.new LibZMQ::PollItem, socks.count, true
+      @poll_structs = FFI::MemoryPointer.new LibZMQ::PollItem, @socks.count, true
       
       # Create the PollItem objects.
       # Initializing them within the FFI::MemoryPointer prevents having to copy
       # the struct data to the MemoryPointer when polling, then back again to
       # retrieve the revents flags.
       i = 0
-      socks.each do |socket, events|
-        poll_items.push LibZMQ::PollItem.new(poll_pointer_array[i]).tap { |pi|
+      @socks.each do |socket, events|
+        @poll_items.push LibZMQ::PollItem.new(@poll_structs[i]).tap { |pi|
           pi.socket = socket
           pi.events = events
         }
         
         i += 1
       end
-      
+    end
+    
+    # Start polling.
+    # 
+    # Returns a hash of ZMQ::Socket => revents (triggered event flags).
+    # Each item of the hash is passed to the block, if provided.
+    def run(&block)
       # Convert seconds to miliseconds.
       timeout = (@timeout * 1000).to_i if @timeout > 0
       
       # Poll
-      rc = LibZMQ::zmq_poll poll_pointer_array, poll_items.count, timeout
+      rc = LibZMQ::zmq_poll @poll_structs, @poll_items.count, timeout
       ZMQ.error_check true if rc==-1
       
       # Create a hash of the items with triggered events.
       # (ZMQ::Socket => revents)
-      triggered_items = poll_items.select { |pi| pi.revents > 0 }
-        .map { |pi| [socket_lookup[pi.socket.address], pi.revents] }
+      triggered_items = @poll_items.select { |pi| pi.revents > 0 }
+        .map { |pi| [@socket_lookup[pi.socket.address], pi.revents] }
         .to_h
       
       # Pass triggered sockets to block.
       triggered_items.each { |socket, revents| block.call socket, revents }
       
       triggered_items
-    end
-    
-    # Start polling.
-    def run
     end
     
   end
