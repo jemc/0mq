58274c3e3bda5cf9b41cdf8c39601fd196a9a91d
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-05-27 23:27:08 -0700

    Opt-out of curve spec if not compiled with libsodium

--- spec/curve_spec.rb
+++ spec/curve_spec.rb
@@ -6,7 +6,12 @@ describe ZMQ::Curve do
   it "can generate a keypair" do
     pending 'Unsupported in libzmq < 4' if ZMQ.version < 4
     
-    keypair = ZMQ::Curve.keypair
+    keypair = begin
+      ZMQ::Curve.keypair
+    rescue Errno::EOPNOTSUPP
+      pending 'Compiled without libsodium'
+    end
+    
     keypair[:public].size.should eq  40
     keypair[:private].size.should eq 40
   end
