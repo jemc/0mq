9108329a71c2797dfe7d7f085957fbe166cb3498
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-05-30 23:19:35 -0700

    Add guard in PollInterruptible specs to close if the spec itself did not
    --HG--
    extra : amend_source : c75ed8575569708d001e7f362eb7372cf53bcefb

--- spec/poll_interruptible_spec.rb
+++ spec/poll_interruptible_spec.rb
@@ -10,68 +10,72 @@ describe ZMQ::PollInterruptible do
   
   it_behaves_like "a poll class"
   
-  let!(:never_sock) { ZMQ::Socket.new(ZMQ::PULL) } # A socket never used
-  subject { poll_class.new never_sock }
-  
-  around { |test| Timeout.timeout(1) {test.run} } # Timeout after 1 second
-  
-  it "can be interrupted" do
-    subject
-    interruptions = []
-    run_proc = ->(*args){ interruptions << args }
+  describe do
+    let!(:never_sock) { ZMQ::Socket.new(ZMQ::PULL) } # A socket never used
+    subject { poll_class.new never_sock }
     
-    3.times do
-      Thread.new { subject.run &run_proc }
-            .tap { subject.interrupt.should eq true }
-            .join
+    around { |test| Timeout.timeout(1) {test.run} } # Timeout after 1 second
+    
+    after { subject.close unless subject.dead? }
+    
+    it "can be interrupted" do
+      subject
+      interruptions = []
+      run_proc = ->(*args){ interruptions << args }
+      
+      3.times do
+        Thread.new { subject.run &run_proc }
+              .tap { subject.interrupt.should eq true }
+              .join
+      end
+      
+      interruptions.count.should eq 3
+      interruptions.each { |sock,evts| sock.should eq nil; evts.should eq nil }
     end
     
-    interruptions.count.should eq 3
-    interruptions.each { |sock,evts| sock.should eq nil; evts.should eq nil }
-  end
-  
-  it "can be killed" do
-    subject
-    subject.dead?.should eq false
+    it "can be killed" do
+      subject
+      subject.dead?.should eq false
+      
+      Thread.new {
+        subject.run { |sock,evts| sock.should eq nil; evts.should eq nil }
+      }.tap { subject.kill.should eq true }.join
+      
+      # Poll is now dead
+      subject.dead?.should eq true
+      expect { subject.run }.to raise_error RuntimeError
+      subject.kill.should eq nil # return nil if already dead
+      subject.close.should eq nil # return nil if already dead
+    end
     
-    Thread.new {
-      subject.run { |sock,evts| sock.should eq nil; evts.should eq nil }
-    }.tap { subject.kill.should eq true }.join
+    it "can be closed" do
+      subject
+      subject.dead?.should eq false
+      
+      Thread.new { subject.run }.tap { subject.interrupt }.join
+      
+      subject.close.should eq true
+      
+      # Poll is now dead
+      subject.dead?.should eq true
+      expect { subject.run }.to raise_error RuntimeError
+      subject.kill.should eq nil # return nil if already dead
+      subject.close.should eq nil # return nil if already dead
+    end
     
-    # Poll is now dead
-    subject.dead?.should eq true
-    expect { subject.run }.to raise_error RuntimeError
-    subject.kill.should eq nil # return nil if already dead
-    subject.close.should eq nil # return nil if already dead
-  end
-  
-  it "can be closed" do
-    subject
-    subject.dead?.should eq false
-    
-    Thread.new { subject.run }.tap { subject.interrupt }.join
-    
-    subject.close.should eq true
-    
-    # Poll is now dead
-    subject.dead?.should eq true
-    expect { subject.run }.to raise_error RuntimeError
-    subject.kill.should eq nil # return nil if already dead
-    subject.close.should eq nil # return nil if already dead
-  end
-  
-  it "can be instantiated and used with no sockets given" do
-    subject = poll_class.new()
-    
-    Thread.new { subject.run }.tap { subject.interrupt.should eq true }.join
-    Thread.new { subject.run }.tap { subject.interrupt.should eq true }.join
-    Thread.new { subject.run }.tap { subject.kill.should eq true }.join
-    
-    # Poll is now dead
-    subject.dead?.should eq true
-    expect { subject.run }.to raise_error RuntimeError
-    subject.kill.should eq nil # return nil if already dead
-    subject.close.should eq nil # return nil if already dead
+    it "can be instantiated and used with no sockets given" do
+      subject = poll_class.new()
+      
+      Thread.new { subject.run }.tap { subject.interrupt.should eq true }.join
+      Thread.new { subject.run }.tap { subject.interrupt.should eq true }.join
+      Thread.new { subject.run }.tap { subject.kill.should eq true }.join
+      
+      # Poll is now dead
+      subject.dead?.should eq true
+      expect { subject.run }.to raise_error RuntimeError
+      subject.kill.should eq nil # return nil if already dead
+      subject.close.should eq nil # return nil if already dead
+    end
   end
   
 end
