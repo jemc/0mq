8c77d3e82cf5c67cc2a182d8cda6c156631645d6
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-06-03 23:33:10 -0700

    Bump rspec to 3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,27 @@
 
-$IN_DEVELOPMENT = true
+require '0mq'
 
-# Pry into the context of exceptions and failures
-# require 'pry-rescue/rspec' if $IN_DEVELOPMENT
+require 'rspec/its'
 require 'timeout'
+require 'pry'
+# require 'pry-rescue/rspec'
 
 Thread.abort_on_exception = true
 
 RSpec.configure do |c|
-  if $IN_DEVELOPMENT
-    
-    # If any tests are marked with iso:true, only run those tests
-    c.filter_run_including iso:true
-    c.run_all_when_everything_filtered = true
-    
-    # Abort after first failure
-    c.fail_fast = true
-  end
+  # Enable 'should' syntax
+  c.expect_with(:rspec) { |c| c.syntax = [:should, :expect] }
+  c.mock_with(:rspec)   { |c| c.syntax = [:should, :expect] }
+  
+  # If any tests are marked with iso:true, only run those tests
+  c.filter_run_including iso:true
+  c.run_all_when_everything_filtered = true
+  
+  # Abort after first failure
+  # (Use environment variable for developer preference)
+  c.fail_fast = true if ENV['RSPEC_FAIL_FAST']
   
   # Set output formatter and enable color
   c.formatter = 'Fivemat'
   c.color     = true
 end
-
-require '0mq'
