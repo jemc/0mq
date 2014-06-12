38b607375ff5863bcf19bf0ab5f762bb6bd8ecf8
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2014-06-11 18:20:06 -0700

    Handle "Native 0MQ error codes" as well as "real" errno codes

--- lib/0mq/error_check.rb
+++ lib/0mq/error_check.rb
@@ -1,6 +1,31 @@
 
 module ZMQ
   
+  # "Native 0MQ error codes"
+  # Each is defined in zmq.h and is not a standard errno code
+  # Description for each error code is paraphrased from the zmq man pages
+  {
+    :EFSM           => "Finite state machine is in the wrong state",
+    :ENOCOMPATPROTO => "The requested protocol is not compatible",
+    :ETERM          => "The context was terminated",
+    :EMTHREAD       => "No I/O thread is available to accomplish the task",
+  }.each do |name, message|
+    klass = Class.new SystemCallError do
+      define_singleton_method :exception do |*args|
+        super(*args)
+        .tap { |exc| exc.define_singleton_method(:message) { message } }
+        .tap { |exc| exc.define_singleton_method(:to_s)    { message } }
+        .tap { |exc| exc.define_singleton_method(:inspect) { 
+          "#<Errno::#{name}: #{message}>"
+        } }
+      end
+    end
+    
+    klass.const_set :Errno, ZMQ.const_get(name)
+    Errno.const_set name, klass
+  end
+  
+  
   # A hash of error number => exception class.
   # Example: 1 => Errno::EPERM
   @error_map = Hash.new
--- spec/error_check_spec.rb
+++ spec/error_check_spec.rb
@@ -9,9 +9,18 @@ describe 'ZMQ.error_check' do
     expect { ZMQ.error_check }.to raise_error Errno::EPERM
   end
   
-  it "raises SystemCallError with errno shown if errno is unrecognized" do
-    LibZMQ.should_receive(:zmq_errno) { 156384763 }
-    expect { ZMQ.error_check }.to raise_error SystemCallError, /156384763/
+  it "raises the correct SystemCallError for ZMQ-specific codes" do
+    LibZMQ.should_receive(:zmq_errno) { ZMQ::EFSM }
+    expect { ZMQ.error_check }.to raise_error Errno::EFSM, /wrong state/
+    
+    LibZMQ.should_receive(:zmq_errno) { ZMQ::ENOCOMPATPROTO }
+    expect { ZMQ.error_check }.to raise_error Errno::ENOCOMPATPROTO, /protocol/
+    
+    LibZMQ.should_receive(:zmq_errno) { ZMQ::ETERM }
+    expect { ZMQ.error_check }.to raise_error Errno::ETERM, /terminated/
+    
+    LibZMQ.should_receive(:zmq_errno) { ZMQ::EMTHREAD }
+    expect { ZMQ.error_check }.to raise_error Errno::EMTHREAD, /I\/O thread/
   end
   
 end
